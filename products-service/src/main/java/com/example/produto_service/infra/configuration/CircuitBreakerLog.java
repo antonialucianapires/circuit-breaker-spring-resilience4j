2fa395e959dee15c6340d25614610c44e45ebfd3
Antonia Luciana Pires <antonialucianapires@gmail.com>
2024-07-22 01:03:00 +0000

    config: add log to track CircuitBreaker status changes

---
+++ products-service/src/main/java/com/example/produto_service/infra/configuration/CircuitBreakerLog.java
@@ -0,0 +1,37 @@
+package com.example.produto_service.infra.configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Log4j2
+@Configuration
+public class CircuitBreakerLog {
+
+    @Bean
+    public RegistryEventConsumer<CircuitBreaker> registryEventConsumer() {
+        return new RegistryEventConsumer<CircuitBreaker>() {
+            @Override
+            public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry().getEventPublisher().onStateTransition(event -> {
+                    log.info("Circuit breaker state transition: {}", event);
+                });
+            }
+
+            @Override
+            public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+
+            }
+
+            @Override
+            public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+
+            }
+        };
+    }
+}
